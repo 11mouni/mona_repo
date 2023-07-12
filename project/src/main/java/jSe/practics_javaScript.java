2a35ca8414c09fd2a860f19cbb34f42f0a5a9940
11mouni <138847085+11mouni@users.noreply.github.com>
2023-07-12 16:16:50 +0530

    commit mouse action

--- project/src/main/java/jSe/practics_javaScript.java
+++ project/src/main/java/jSe/practics_javaScript.java
@@ -1,20 +1,41 @@
 package jSe;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
 
 public class practics_javaScript {
 
-	public static void main(String[] args) {
 	
+	@Test
+	public void mouseactions() {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		
-		JavascriptExecutor js = (JavascriptExecutor) driver;
-		WebElement element = (WebElement) js.executeScript("");
-
+		driver.get("https://www.orangehrm.com/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		By xpath_why_orange = By.xpath("//a[text()='Why OrangeHRM']");
+		By xpath_outcustomes =By.xpath("//li[normalize-space(text()) = 'Our Customers']");
+		By xpath_testmonial =By.xpath("(//a[text()='Testimonials'])[1]");
+		
+		WebElement why_org_web =driver.findElement(xpath_why_orange);
+		WebElement our_cus_web =driver.findElement(xpath_outcustomes);
+		WebElement test_web = driver.findElement(xpath_testmonial);
+		
+		Actions act = new Actions(driver);
+		act.moveToElement(why_org_web)
+		.moveToElement(our_cus_web)
+		.moveToElement(test_web)
+		.click()
+		.perform();
+		
 	}
 
 }
