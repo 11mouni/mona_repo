656cd09cd77d97b63e702c2fdc37a1c9b787d281
11mouni <138847085+11mouni@users.noreply.github.com>
2023-07-14 11:43:07 +0530

    commit

--- project/src/main/java/jSe/conflict.java
+++ project/src/main/java/jSe/conflict.java
@@ -3,9 +3,9 @@ package jSe;
 public class conflict {
 
 	public static void main(String[] args) {
-		int a =10;
-		int b= 20;
-		int sum = a+b;
+		int X1 =10;
+		int Y1= 20;
+		int sum = X1+Y1;
 		System.out.println(sum);
 
 	}
